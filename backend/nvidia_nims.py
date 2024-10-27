54bdd685586d4655e8857055ffad71e0f6544963
Ashish Soni <ashish.soni2091@gmail.com>
2024-10-27 22:49:37 +0000

    nim's

--- backend/nvidia_nims.py
+++ backend/nvidia_nims.py
@@ -2,51 +2,26 @@ from dotenv import dotenv_values
 
 from llama_index.llms.nvidia import NVIDIA
 from llama_index.embeddings.nvidia import NVIDIAEmbedding
-from llama_index.core.postprocessor.nvidia_rerank import NVIDIAReRank
-from llama_index.core.llms import ChatMessage, MessageRole
+from llama_index.postprocessor.nvidia_rerank import NVIDIARerank
 
 from constants import (EMBEDDING_MODEL,
                        LLM_MODEL,
-                       RERANK_MODEL
-                       )
+                       RERANK_MODEL)
 
 config = dotenv_values(".env")
 
+# llama-3.2-nv-embedqa-1b-v1
+embedding_model = NVIDIAEmbedding(base_url=config["NVIDIA_URL"], 
+                                  api_key=config["EMBED_MODEL_API_KEY"],
+                                  model=EMBEDDING_MODEL,
+                                  truncate="NONE")
+
+# llama-3.2-1b-instruct
 llm = NVIDIA(base_url=config["NVIDIA_URL"],
              api_key=config["LLM_API_KEY"],
              model=LLM_MODEL)
 
-messages = [
-    ChatMessage(
-        role=MessageRole.SYSTEM, content=("You are a helpful assistant.")
-    ),
-    ChatMessage(
-        role=MessageRole.USER,
-        content=("What is your name?"),
-    ),
-]
-
-response = llm.stream_chat(messages)
-print(response)
-
-last_element = None
-for last_element in response:
-    pass
-
-print(last_element)
-
-def get_embeddings():
-
-    
-    return None
-
-embedding_model = NVIDIAEmbedding(base_url=config["NVIDIA_URL"], 
-                                  api_key=config["EMBED_MODEL_API_KEY"],
-                                  model=EMBEDDING_MODEL,
-                                  truncate="NONE"
-                                  )
-
-rerank_model = NVIDIAReRank(base_url=config["NVIDIA_RERANK_URL"],
-                            api_key=config["RERANK_MODEL_API_KEY"],
-                            model=RERANK_MODEL
-                            )
+# llama-3.2-nv-embedqa-1b-v1
+rerank_model = NVIDIARerank(api_key=config["RERANK_MODEL_API_KEY"],
+                            model=RERANK_MODEL,
+                            top_n=5)
